be232d4e7f149af17270c55d5e69d8beb5c8111c
Isaiah <raustisaiah@gmail.com>
2025-12-01 15:33:16 -0600

    File energydrink patch

---
+++ src/MasterMode/Patches/EnergyDrinkPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace MasterMode.Patches
+{
+    [HarmonyPatch(typeof())]
+    internal class EnergyDrinkPatch
+    {
+
+        [HarmonyPatch(nameof())]
+        [HarmonyPrefix]
+        private static void ()
+        {
+
+        }
+
+    }
+
+}
+
